d8271cd84a77b5aaa022694ffdfa0b37c1de193f
JoseCortezKinal <josecortezkinal@gmail.com>
2024-05-22 11:50:06 -0600

    Se implento webSocket

--- src/main/java/org/carloscortez/blogexk2024/WebSocket/WebSocketConfiguration.java
+++ src/main/java/org/carloscortez/blogexk2024/WebSocket/WebSocketConfiguration.java
@@ -1,11 +1,27 @@
 package org.carloscortez.blogexk2024.WebSocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfiguration {
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer{
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/blog-app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/posts-socket").setAllowedOrigins("http://localhost:4200").withSockJS();
+    }
+
+    
 
 }
--- src/main/java/org/carloscortez/blogexk2024/controller/PostController.java
+++ src/main/java/org/carloscortez/blogexk2024/controller/PostController.java
@@ -3,6 +3,10 @@ package org.carloscortez.blogexk2024.controller;
 import org.carloscortez.blogexk2024.model.Post;
 import org.carloscortez.blogexk2024.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
@@ -11,9 +15,11 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+@Controller
 @RestController
 @RequestMapping("blog-app")
-@CrossOrigin(value = "http://localhost:4200/")
+// @CrossOrigin(value = "http://192.168.42.153:4200/") RED LAN
+@CrossOrigin(origins = "http://localhost:4200/" , allowCredentials = "true")
 public class PostController {
 
     @Autowired
@@ -24,10 +30,12 @@ public class PostController {
         return this.postService.listarPost();
     }
 
-    @PostMapping("/posts")
-    public Post crearPost(@RequestBody Post post) {
+    @MessageMapping("/posts-socket/{roomId}")
+    @SendTo("/topic/{roomId}")
+    public Post posts(@DestinationVariable String roomId, Post post){
         post.setPostDate(Date.valueOf(LocalDate.now()));
         post.setPostTime(Time.valueOf(LocalTime.now()));
         return this.postService.GuardarPost(post);
     }
 }
+
